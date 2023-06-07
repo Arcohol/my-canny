23bdbcfd26d73a370993d2db7263be8416bf917d
Tiantian Li <i@arcohol.com>
2023-06-08 03:28:03 +0800

    THE FINAL OF FINAL

--- test.py
+++ test.py
@@ -1,16 +1,21 @@
 import my_canny as my
 import cv2
 
-image = cv2.imread("images/lena.png")
+image = cv2.imread("images/lena.png", 0)
 
-# iterate over the thresholds
-# low_threshold is 1/2 of high_threshold
-for i in range(1, 256):
-    canny_image = my.canny(image, i / 2, i)
-    cv2.imwrite("images/lena/2x/{:03d}.png".format(i), canny_image)
+def test_canny(image, start, stop):
+    blur = my.gaussian_blur(image)
+    gradient, direction = my.sobel(blur)
+    non_max = my.non_max_suppresion_with_interpolation(gradient, direction)
 
-# iterate over the thresholds
-# low_threshold is 1/3 of high_threshold
-for i in range(1, 256):
-    canny_image = my.canny(image, i / 3, i)
-    cv2.imwrite("images/lena/3x/{:03d}.png".format(i), canny_image)
+    for i in range(start, stop):
+        weak, strong = my.double_threshold(non_max, i / 2, i)
+        canny_image = my.hysteresis(weak, strong)
+        cv2.imwrite("images/lena/2x/{:03d}.png".format(i), canny_image)
+
+        weak, strong = my.double_threshold(non_max, i / 3, i)
+        canny_image = my.hysteresis(weak, strong)
+        cv2.imwrite("images/lena/3x/{:03d}.png".format(i), canny_image)
+
+
+test_canny(image, 1, 256)
